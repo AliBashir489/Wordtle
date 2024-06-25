244a139d2e665edabc8f20c69dae823f712c5c3b
AliBashir489 <alithebossx489@gmail.com>
2024-06-25 12:00:54 -0400

    Added wooden design in background and fixed some UI elements to make game visually apealing. Also made boxes nicer and improved convent view pageAdded the legend of the box colors for player to know what each color represents, although maybe this should be added to content view instead of gameview.

--- WordtleGame/ContentView.swift
+++ WordtleGame/ContentView.swift
@@ -1,31 +1,45 @@
-
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            VStack {
-                Spacer()
-                Text("WORDTLE")
-                    .padding()
-                    .bold()
-                    .font(.system(size: 50, weight: .heavy, design: .default))
-                    .foregroundColor(.blue)
+            ZStack{
+                // Background image
+                Image("1")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                    .rotationEffect(.degrees(90))
+                    .opacity(0.925)
                 
-                Spacer()
-                NavigationLink(destination: GameView()) {
-                    Text("Start Game")
-                        .font(.title)
+                VStack {
+                    Spacer()
+                    Text("Wordtle")
                         .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(15)
+                        .bold()
+                        .font(.custom("Courier", size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.black)
+                        .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                        .overlay(
+                            Rectangle()
+                                .stroke(Color.black, lineWidth: 5))
+                    
+                    
+                    Spacer()
+                    NavigationLink(destination: GameView()) {
+                        Text("Start Game")
+                            .font(.title)
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(20)
+                    }
+                    
+                    Spacer()
                 }
                 
-                Spacer()
             }
-            
         }
     }
 }
--- WordtleGame/GameView.swift
+++ WordtleGame/GameView.swift
@@ -3,45 +3,88 @@ import SwiftUI
 struct GameView: View {
     // Defines the grid for the letters it is 6 rows and 5 columns
     @State private var grid: [[String]] = Array(repeating: Array(repeating: "", count: 5), count: 6)
-   
+    
     
     // Tracks the current row and column
     @State private var currentRow = 0
     @State private var currentColumn = 0
     
     var body: some View {
-        VStack {
-            Spacer()
-            // Display the grid of letter boxes
-            VStack(spacing: 10) {
-                ForEach(0..<6, id: \.self) { row in
-                    HStack(spacing: 10) {
-                        ForEach(0..<5, id: \.self) { column in
-                            Text(grid[row][column])
-                                .frame(width: 40, height: 40)
-                                .background(Color.gray.opacity(0.2))
-                                .border(Color.black, width: 2)
-                                .font(.title)
-                                .multilineTextAlignment(.center)
+        ZStack {
+            // Background image
+            Image("1")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+                .rotationEffect(.degrees(90))
+                .opacity(0.925)
+            VStack {
+                Spacer()
+                Text("Guess the 5 Letter Word")
+                    .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                    .padding()
+                    .bold()
+                    .font(.custom("Courier", size: 25))
+                    .fontWeight(.heavy)
+                    .foregroundColor(.black)
+                Text("GREEN = CORRECT PLACE, CORRECT LETTER")
+                    
+                    .bold()
+                    .font(.custom("Courier", size: 16))
+                    .fontWeight(.heavy)
+                    .foregroundColor(.black)
+                Text("YELLOW = WRONG PLACE, CORRECT LETTER")
+                    
+                    .bold()
+                    .font(.custom("Courier", size: 16))
+                    .fontWeight(.heavy)
+                    .foregroundColor(.black)
+                Text("GREY = WRONG PLACE, WRONG LETTER")
+                    
+                    .bold()
+                    .font(.custom("Courier", size: 16))
+                    .fontWeight(.heavy)
+                    .foregroundColor(.black)
+                
+                Spacer()
+                Spacer()
+                // Display the grid of letter boxes
+                VStack(spacing: 15) {
+                    ForEach(0..<6, id: \.self) { row in
+                        HStack(spacing: 10) {
+                            ForEach(0..<5, id: \.self) { column in
+                                Text(grid[row][column])
+                                    .frame(width: 45, height: 45)
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 5)
+                                            .stroke(Color.black, lineWidth: 3)
+                                            .background(Color.gray.opacity(0.35))
+                                        )
+                                    .font(.title)
+                                    .bold()
+                                    .multilineTextAlignment(.center)
+                            }
                         }
                     }
                 }
-            }
-            Spacer()
-            
-            //this diplays the keybaord
-            VStack(spacing: 10) { //
-                ForEach(["QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"], id: \.self) { row in
-                    HStack(spacing: 5) {
-                        ForEach(row.map { String($0) }, id: \.self) { key in
-                            Button(action: {
-                                handleKeyPress(key: key)
-                            }) {
-                                Text(key)
-                                    .frame(width: 30, height: 40)
-                                    .background(Color.blue)
-                                    .foregroundColor(.white)
-                                    .cornerRadius(5)
+                
+                Spacer()
+                Spacer()
+                
+                //this diplays the keybaord
+                VStack(spacing: 10) { //
+                    ForEach(["QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"], id: \.self) { row in
+                        HStack(spacing: 5) {
+                            ForEach(row.map { String($0) }, id: \.self) { key in
+                                Button(action: {
+                                    handleKeyPress(key: key)
+                                }) {
+                                    Text(key)
+                                        .frame(width: 32, height: 44)
+                                        .background(Color.blue)
+                                        .foregroundColor(.white)
+                                        .cornerRadius(5)
+                                }
                             }
                         }
                     }
@@ -49,7 +92,7 @@ struct GameView: View {
             }
         }
         .padding()
-        .navigationTitle("Wordtle")
+        .navigationTitle(" ")
     }
     
     private func handleKeyPress(key: String) {
