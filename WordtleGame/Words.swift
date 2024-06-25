978a6e43baaa0e5397d50c85a43cc43329556cc2
AliBashir489 <alithebossx489@gmail.com>
2024-06-25 18:11:22 -0400

    Added color schemes and if player gets right letter in right place then the box turns green and if a correct lette rin the wrong place then it turns yellow. if nothing then grey. when the board is full, game resets. now need to add win or lose message. also made a serperate file with data for the random words. the game randomly gets a 5 letter word from the list in the other file. thats it for this commit.

---
+++ WordtleGame/Words.swift
@@ -0,0 +1,18 @@
+//
+//  Words.swift
+//  WordtleGame
+//
+//  Created by Ali Bashir on 6/25/24.
+//
+
+import Foundation
+
+
+let fiveLetterWords = [
+    "APPLE", "TABLE", "PHONE", "HOUSE", "WATER",
+    "GREEN", "CHAIR", "PLANT", "SMILE", "BEACH",
+    "DREAM", "PIZZA", "STONE", "BREAD", "TIGER",
+    "RIVER", "SHOES", "HEART", "HONEY", "MUSIC",
+    "LEMON", "SOCKS", "PAPER", "RULER", "CANDY",
+    "PEACH", "CLOWN", "CLOUD", "SOCKS", "CHAIR"
+]
